927beddd2d50de6a7ba3170942221ed3c6caeddb
Twilighters <ilya475@rambler.ru>
2021-08-07 22:14:53 +0300

    validAll main app

--- main.py
+++ main.py
@@ -1,42 +1,67 @@
-from json.decoder import JSONDecodeError
-import logging
-import json
-
-log = logging.getLogger(__name__)
-
-class InputError(Exception):
+class InputParameterVerificationError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(message)
+
     def __str__(self):
         return f'ошибка: {self.message}'
 
-class MoveError(Exception):
+
+class ResultVerificationError(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(message)
+
     def __str__(self):
-        return f'ошибка : {self.message}'
+        return f'ошибка: {self.message}'
 
-def check_move(matrix, move):
-    return True
+user_input1 = 12
+user_input2 = 2
+on_fail_repeat_times = 1
 
-def check_input(move):
-    return False
+def input_validation(user_input1, user_input2):
+    if isinstance(user_input1, int):
+        return True
+    if isinstance(user_input2, int):
+        return True
+    else:
+        return False
 
-def checkmoving(_input):
-    try:
-        matrix = 0
-        Command = json.loads(_input)
-        if not check_input(Command):
-            raise InputError("сработала ошибка")
-        if not check_move(matrix, Command):
-            raise MoveError("сработала вторая ошибка")
-    except JSONDecodeError as err:
-        log.error("Ошибка импорта")
-    except InputError as err:
-        log.error(f"Ошибка ввода {err}")
-    except MoveError as err:
-        log.error("Предел карты превышен")
 
-checkmoving("{}")
+def result_validation(check_difference):
+    if check_difference >= 0:
+    #     return True
+    # else:
+        return False
+
+def default_behavior ():
+    return None
+
+def my_decorator(input_validation, on_fail_repeat_times, result_validation):
+    def decoration(func):
+        def wrapper(*args):
+            if not input_validation(user_input1, user_input2):
+                raise InputParameterVerificationError("Должны быть только цифры")
+            print("то что происходит ДО вызова функции")
+            for i in range(on_fail_repeat_times):
+                result = func(*args)
+                check_difference = result
+            print("то что происходит ПОСЛЕ вызова функции")
+            if not result_validation(check_difference):
+                if on_fail_repeat_times < 3:
+                    raise ResultVerificationError("Ошибка проверки результата")
+            return result
+
+        return wrapper
+
+    return decoration
+
+
+@my_decorator(input_validation, on_fail_repeat_times, result_validation)
+def difference(user_input1, user_input2):
+    result = user_input1 - user_input2
+    print(user_input1, user_input2, "То что происходит ВО ВРЕМЯ исполнения функции")
+    return result
+
+
+difference(user_input1, user_input2)
